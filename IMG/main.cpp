fcc44452d7074961f7284ed9674a13b42cadd149
Vicenç Gascó <vicenc.gasco@gmail.com>
2011-05-07 15:19:54 +0200

    Initial Commit

---
+++ IMG/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  IMG
+//
+//  Created by enwine on 5/7/11.
+//  Copyright 2011 Universitat Pompeu Fabra. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
